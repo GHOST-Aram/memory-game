3dcefcc9cd71bc1cdd437f4d5f407549b5f16c32
GHOST-Aram <owinoojala@gmail.com>
2023-04-25 08:18:48 +0300

    chore: Render individual images inside a div element

--- src/components/image-grid.js
+++ src/components/image-grid.js
@@ -1,13 +1,13 @@
 import uniqid from 'uniqid'
-import image1 from './images/tesla2.jfif'
-import image2 from './images/tesla3.jfif'
-import image5 from './images/tesla6.jfif'
-import image6 from './images/tesla7.jfif'
-import image7 from './images/tesla8.jfif'
-import image8 from './images/tesla9.jfif'
-import image9 from './images/tesla10.jfif'
-import image10 from './images/tesla11.jfif'
-import image12 from './images/tesla1.jfif'
+import image1 from './images/tesla2.png'
+import image2 from './images/tesla4.png'
+import image5 from './images/tesla6.png'
+import image6 from './images/tesla7.png'
+import image7 from './images/tesla8.png'
+import image8 from './images/tesla9.png'
+import image9 from './images/tesla10.png'
+import image10 from './images/tesla11.png'
+import image12 from './images/tesla1.png'
 
 import { reshuffleImages } from '../data'
 
@@ -33,15 +33,22 @@ export const ImageGrid = () => {
     const [images, setImages] = useState(collection)
     
 
-    const handleClick = () => {
-      // e.target.style.display = 'none'
+    const handleClick = (e) => {
+      e.target.style.display = 'none'
       setImages(reshuffleImages(collection))
     }
     
   
   return (
-    <div className='grid grid-4 gap-4 container'>
-      {images.map((image, index) => <img  id= {image.id} onClick={handleClick} key={image.id} src ={image.src}  alt={`imageNo-${index + 1}`}/>)}
+    <div className='grid grid-4 gap-4 container py-12'>
+      {
+      images.map((image, index) => {
+        return <div className='p-4 bg-gray rounded-md'>
+                  <img  id= {image.id} onClick={(e)=>handleClick(e)} 
+                  key={image.id} src ={image.src}  alt={`imageNo-${index + 1}`}/>
+              </div>
+      })
+      }
     </div>
   )
 }
